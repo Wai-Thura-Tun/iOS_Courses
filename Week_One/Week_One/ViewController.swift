f9b623b845d6ab7b88cce5b250baa7c0fe5efaeb
Wai-Thura-Tun <maxw52493@gmail.com>
2025-08-01 10:39:23 +0700

    adding week one uikit

---
+++ Week_One/Week_One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week_One
+//
+//  Created by Wai Thura Tun on 1/8/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
